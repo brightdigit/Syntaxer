5b20f6cfa24cf72fdc039fe3fc158df62d445824
Leo Dion <leogdion@brightdigit.com>
2025-07-05 12:08:23 -0400

    fixing some code

--- working_example.swift
+++ working_example.swift
@@ -1,9 +1,5 @@
-let code = Struct("Person") {
+Struct("Person") {
     Variable(.var, name: "firstName", type: "String")
     Variable(.var, name: "lastName", type: "String")
     Variable(.var, name: "age", type: "Int")
-    
-    Function("fullName") {
-        Return(Literal("\\(firstName) \\(lastName)"))
-    }
 }
